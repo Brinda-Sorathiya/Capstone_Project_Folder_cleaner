d379e1e659f9f34385745bfcd24159f1e507c73b
ValaHardikV <161627696+ValaHardikV@users.noreply.github.com>
2024-04-07 02:36:08 +0530

    Create lexicographical_sorting.cpp

---
+++ Src_code/lexicographical_sorting.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<filesystem>
+#include<chrono>
+#include<sstream>
+#include<string>
+#include<unordered_map>
+#include<vector>
+#include<stack>
+#include<fstream>
+#include<algorithm>
+#include "2D_list.h"
+
+
+namespace fs = std::filesystem;
+using namespace std;
+using namespace std::chrono;
+
+
+void SortedFileAlplabet(m_node* temp){
+
+    while(temp != NULL){
+
+        //check folder path exist or not
+        if ((!fs::exists(temp->fdr)) || !fs::is_directory(temp->fdr)) {
+            cout << "Error: Invalid folder path : temp->fdr" << endl;
+            temp = temp->link;
+            continue;
+        }
+
+        // Vector to store file paths
+        vector <fs::path> filePaths;
+
+        // push all file in vector
+        for (const auto& a : fs::directory_iterator(temp->fdr)) {
+            if (a.is_regular_file()) {
+                filePaths.push_back(a.path());  //push filepath in vector
+            }
+        }
+
+        // Sort file paths lexicographically
+        sort(filePaths.begin(), filePaths.end());
+
+        // Temporary folder to hold sorted files
+        string tempFolder = temp->fdr.string() + "_temp";
+
+        // Create temporary folder if it doesn't exist
+        if (!fs::exists(tempFolder)) {
+            fs::create_directory(tempFolder);
+        }
+
+        // Move files to temporary folder
+        for (const auto& b : filePaths) {
+            string newPath = tempFolder + "/" + b.filename().string();
+            fs::rename(b, newPath);
+        }
+
+        // Move files back to original folder
+        for (const auto& c : fs::directory_iterator(tempFolder)) {
+            fs::rename(c.path(), temp->fdr.string() + "/" + c.path().filename().string());
+        }
+
+        // Remove temporary folder
+        fs::remove_all(tempFolder);
+
+        cout << "Files in the folder " << temp->fdr << " have been sorted lexicographically." << endl;
+
+        temp = temp->link;
+
+    }
+
+    return;
+}
