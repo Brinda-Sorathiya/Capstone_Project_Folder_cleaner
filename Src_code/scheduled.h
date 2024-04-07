e4b054dcd776175b993375522329d6f1aef0f9f3
Mahek-Vaghera <160610567+Mahek-Vaghera@users.noreply.github.com>
2024-04-07 17:36:00 +0530

    Update scheduled.h

--- Src_code/scheduled.h
+++ Src_code/scheduled.h
@@ -4,18 +4,18 @@ using namespace std;
 namespace fs = std::filesystem;
 using namespace std::chrono;
 //Functiont to do a shedule cleaning of a file
-void cleanFilesAfterInterval(const vector<string> &file_paths, int interval_minutes)                          
-{   cout<<"wait for "<<interval_minutes<<" for file deletion:"<<endl; //takes input minutes from user 
+void cleanFilesAfterInterval(const vector<string> &paths, int minutes)                          
+{   cout<<"wait for "<<minutes<<" for file deletion:"<<endl; //takes input minutes from user 
     auto start_time = steady_clock::now(); //shows current time of system
 
     while (true)
     {
-        auto elapsed_time =steady_clock::now() - start_time; //diffrence between system's current time and already stored time
-        auto elapsed_minutes =duration_cast<minutes>(elapsed_time).count(); //convert elapsed time duration into minutes
+        auto time_diffrence =steady_clock::now() - start_time; //diffrence between system's current time and already stored time
+        auto minute_difference =duration_cast<minutes>(time_diffrence).count(); //convert elapsed time duration into minutes
 
-        if (elapsed_minutes >= interval_minutes)
+        if (minute_difference >= minutes)
         {
-            for (const auto &file_path : file_paths) //iterates through vector of file path 
+            for (const auto &file_path : paths) //iterates through vector of file path 
             {
                 if (fs::exists(file_path) && fs::is_regular_file(file_path))  // if file exists and a regular file
                 {
